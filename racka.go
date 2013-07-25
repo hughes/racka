c26696800f690c4af2dbcddb1fd76030d1ed358f
Matt Hughes <hughesm02@gmail.com>
2013-07-24 21:10:45 -0400

    Added some model structs

--- racka.go
+++ racka.go
@@ -2,7 +2,20 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+
+type User struct {
+	pk       int
+	key      string
+	email    string
+	password string
+}
+
+type Day struct {
+	date time.Time
+	hours string
+}
 
 func main() {
 	fmt.Println("Running server...")
